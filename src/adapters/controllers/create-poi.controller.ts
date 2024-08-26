c18210fb7dd43b53733de1933c4a832a61395f19
Filipe Siqueira <filipedev040990@gmail.com>
2024-08-26 17:59:32 -0300

    feat: add get all poi controlelr

--- src/adapters/controllers/create-poi.controller.ts
+++ src/adapters/controllers/create-poi.controller.ts
@@ -1,11 +1,11 @@
-import { ControllerInterface, HttpRequest } from '@/domain/controllers/controller.interface'
+import { ControllerInterface, HttpRequest, HttpResponse } from '@/domain/controllers/controller.interface'
 import { CreatePOIUseCaseInterface } from '@/domain/usecases/create-poi.interface'
 import { handleError } from '@/shared/helpers/error.helper'
 import { success } from '@/shared/helpers/http.helper'
 
 export class CreatePOIController implements ControllerInterface {
   constructor(private readonly createPOIUseCase: CreatePOIUseCaseInterface) {}
-  async execute(input: HttpRequest): Promise<any> {
+  async execute(input: HttpRequest): Promise<HttpResponse> {
     try {
       await this.createPOIUseCase.execute(input?.body)
       return success(204, null)
