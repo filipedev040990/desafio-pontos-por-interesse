c4c26ca37ab2be2e69b9828cf45a4bbef3e6d37b
Filipe Siqueira <filipedev040990@gmail.com>
2024-08-26 18:00:34 -0300

    feat: add get all poi repository

--- src/infra/database/poi.repository.ts
+++ src/infra/database/poi.repository.ts
@@ -1,9 +1,13 @@
 import { POIRepositoryInterface } from '@/domain/repositories/poi.interface'
-import { CreatePOIRepositoryInput } from '@/domain/usecases/poi.types'
+import { CreatePOIRepositoryInput, POIOutput } from '@/domain/usecases/poi.types'
 import { prismaClient } from './prisma-client'
 
 export class POIRepository implements POIRepositoryInterface {
   async save(data: CreatePOIRepositoryInput): Promise<void> {
     await prismaClient.pointOfInterest.create({ data })
+  }
+
+  async getAll(): Promise<POIOutput[] | []> {
+    return await prismaClient.pointOfInterest.findMany()
   }
 }
